fd9d63e7dd6ec1fea35bcd1d2dab7dadb329f92a
janshivemula <janshivemula@gmail.com>
2026-03-18 20:50:15 +0530

    Add files via upload

---
+++ FinalPrice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductDiscount
+{
+    internal class FinalPrice
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(1, "Mobile", 65000, 10);
+            Product p1 = new Product(2, "TV", 70000, 8);
+            p.Display();
+            p1.Display();
+        }
+    }
+}
