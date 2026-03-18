81739155ecfc32f1df46bcd3d9054bf41ba53608
janshivemula <janshivemula@gmail.com>
2026-03-18 20:49:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+
+namespace ProductDiscount
+{
+    class Product
+    {
+        int id;
+        string name;
+        double price;
+        double discount;
+
+        public Product(int id, string n, double p, double d)
+        {
+            this.id = id;
+            this.name = n;
+            this.price = p;
+            this.discount = d;
+
+        }
+        public double PriceAfterDiscount()
+        {
+            discount = price * discount / 100;
+            double finalprice = price - discount;
+            return finalprice;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Id : " + id);
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("ActualPrice : " + price);
+            Console.WriteLine("Discount : " + discount);
+            Console.WriteLine("Price after Discount : " + PriceAfterDiscount());
+
+        }
+            
+    }
+}
